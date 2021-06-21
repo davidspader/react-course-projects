9b0f62f45ed08d7c64ab40aed9f522c68321bc6f
David Spader Ambrozio <davidspader97@gmail.com>
2021-06-21 15:52:07 -0300

    15-cocktails

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe officia
+        ea iste placeat, laudantium aliquam sequi fugit explicabo quasi quos
+        quis provident ullam illo optio libero. Debitis, numquam. Incidunt,
+        beatae?
+      </p>
+    </section>
+  );
+};
+
+export default About;
